5c87df9aee57cd9af4a2cc7fc0b6220cf281a9b2
ucfcount1 <ucfcount1@gmail.com>
2025-09-14 19:23:41 +0100

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// Example usage:
+console.log(sum(5, 3)); // Output: 8
+console.log(sum(10, -2)); // Output: 8
+console.log(sum(2.5, 3.7)); // Output: 6.2
